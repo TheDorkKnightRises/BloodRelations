31ee8e18021282bf9a7d6743cefeb13cb548a6b8
Aditya Desai <adityadesai97@gmail.com>
2017-03-31 13:37:24 +0530

    Okay

---
+++ app/src/main/java/smartindia/santas/bloodrelations/activities/LanguageActivity.java
@@ -0,0 +1,15 @@
+package smartindia.santas.bloodrelations.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import smartindia.santas.bloodrelations.R;
+
+public class LanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language);
+    }
+}
