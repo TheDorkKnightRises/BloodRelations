2b19952802e2d2155d0e1ae72f88772879c070f9
Aditya Desai <adityadesai97@gmail.com>
2017-03-31 14:32:56 +0530

    Merge branch 'master' of /Users/adityadesai/Desktop/BloodRelations with conflicts.

--- app/src/main/java/smartindia/santas/bloodrelations/activities/ProfileActivity.java
+++ app/src/main/java/smartindia/santas/bloodrelations/activities/ProfileActivity.java
@@ -1,24 +1,79 @@
 package smartindia.santas.bloodrelations.activities;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import smartindia.santas.bloodrelations.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final int RC_PHOTO_PICKER = 1;
+
+    private de.hdodenhof.circleimageview.CircleImageView profilePicture;
+    private EditText birthDateEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profilePicture=(de.hdodenhof.circleimageview.CircleImageView)findViewById(R.id.profile_image);
+        birthDateEditText=(EditText) findViewById(R.id.birthday_textview);
+
+        final Calendar myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(myCalendar);
+            }
+
+        };
+
+        birthDateEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ProfileActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        birthDateEditText.setInputType(InputType.TYPE_NULL);
+        birthDateEditText.requestFocus();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId())
         {
+            case android.R.id.home:
+                onBackPressed();
+                break;
             case R.id.menu_save:
                 onSavePressed();
                 break;
@@ -35,5 +90,32 @@ public class ProfileActivity extends AppCompatActivity {
         return true;
     }
 
+    public void getPicture(View v){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpeg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+    }
+
+
+    private void updateLabel(Calendar myCalendar) {
+
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        birthDateEditText.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_PHOTO_PICKER){
+            if(resultCode == RESULT_OK){
+                Uri selectedImageUri = data.getData();
+                Picasso.with(ProfileActivity.this).load(selectedImageUri).into(profilePicture);
+            }
+        }
+
+    }
 
 }
