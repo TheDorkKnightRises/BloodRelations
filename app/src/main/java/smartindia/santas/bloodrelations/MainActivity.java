e3b45e194710eebd352a2265736ecacdaba09f6c
TejasBhitle <bhitletejas@gmail.com>
2017-03-31 10:29:24 +0530

    Drawer Added

--- app/src/main/java/smartindia/santas/bloodrelations/MainActivity.java
+++ app/src/main/java/smartindia/santas/bloodrelations/MainActivity.java
@@ -1,13 +1,51 @@
 package smartindia.santas.bloodrelations;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout= (DrawerLayout)findViewById(R.id.drawer_layout) ;
+        navigationView = (NavigationView) findViewById(R.id.nvView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.drawer_open,R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        setupDrawer();
+
+
+    }
+
+    private void setupDrawer(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+
+                }
+                return true;
+            }
+        });
     }
 }
