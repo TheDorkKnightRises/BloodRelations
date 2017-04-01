0a9d2ad2c1f527c4f88ce3fdf9ec43a79664a590
Aditya Desai <adityadesai97@gmail.com>
2017-04-01 14:20:27 +0530

    Pushing profile data and picture done

--- app/src/main/java/smartindia/santas/bloodrelations/activities/ProfileActivity.java
+++ app/src/main/java/smartindia/santas/bloodrelations/activities/ProfileActivity.java
@@ -3,6 +3,9 @@ package smartindia.santas.bloodrelations.activities;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -14,6 +17,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
@@ -27,7 +38,24 @@ public class ProfileActivity extends AppCompatActivity {
     private static final int RC_PHOTO_PICKER = 1;
 
     private de.hdodenhof.circleimageview.CircleImageView profilePicture;
+    private FloatingActionButton profileEditDone;
+
+    private TextInputLayout profileName;
+    private TextInputLayout profileSurname;
+    private TextInputLayout profileEmail;
+    private TextInputLayout profilePhone;
+    private TextInputLayout profileBloodGroup;
+    private TextInputLayout profileAddress;
     private EditText birthDateEditText;
+
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseStorage firebaseStorage;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    private FirebaseUser user;
+
+    private Uri imageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +64,13 @@ public class ProfileActivity extends AppCompatActivity {
 
         profilePicture=(de.hdodenhof.circleimageview.CircleImageView)findViewById(R.id.profile_image);
         birthDateEditText=(EditText) findViewById(R.id.birthday_edittext);
+        profileEditDone=(FloatingActionButton)findViewById(R.id.profile_edit_done_fab);
+        profileName = (TextInputLayout)findViewById(R.id.profile_name);
+        profileSurname = (TextInputLayout)findViewById(R.id.profile_surname);
+        profileEmail = (TextInputLayout)findViewById(R.id.profile_email);
+        profilePhone = (TextInputLayout)findViewById(R.id.profile_phone);
+        profileBloodGroup = (TextInputLayout)findViewById(R.id.profile_blood_group);
+        profileAddress = (TextInputLayout)findViewById(R.id.profile_address);
 
         final Calendar myCalendar = Calendar.getInstance();
 
@@ -65,6 +100,36 @@ public class ProfileActivity extends AppCompatActivity {
         });
         birthDateEditText.setInputType(InputType.TYPE_NULL);
         birthDateEditText.requestFocus();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        storageReference = firebaseStorage.getReference().child("userPhotos");
+
+        profileEditDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("firstname");
+                databaseReference.setValue(profileName.getEditText().getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("surname");
+                databaseReference.setValue(profileSurname.getEditText().getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("email");
+                databaseReference.setValue(profileEmail.getEditText().getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("phone");
+                databaseReference.setValue(profilePhone.getEditText().getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("bloodgroup");
+                databaseReference.setValue(profileBloodGroup.getEditText().getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("address");
+                databaseReference.setValue(profileAddress.getEditText().getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("birthdate");
+                databaseReference.setValue(birthDateEditText.getText().toString());
+                databaseReference = firebaseDatabase.getReference().child("users").child(user.getUid()).child("details").child("imageURL");
+                databaseReference.setValue(imageUrl.toString());
+
+            }
+        });
     }
 
     @Override
@@ -111,8 +176,19 @@ public class ProfileActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == RC_PHOTO_PICKER){
             if(resultCode == RESULT_OK){
-                Uri selectedImageUri = data.getData();
-                Picasso.with(ProfileActivity.this).load(selectedImageUri).into(profilePicture);
+                final Uri selectedImageUri = data.getData();
+                StorageReference photoRef = storageReference.child(selectedImageUri.getLastPathSegment());
+
+                photoRef.putFile(selectedImageUri)
+                        .addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // When the image has successfully uploaded, we get its download URL
+                                imageUrl = taskSnapshot.getDownloadUrl();
+                                // Set the download URL to the message box, so that the user can send it to the database
+                                //Bitmap bitmap = MediaStore.Images.Media.getBitmap(this,imageUrl);
+                                Picasso.with(ProfileActivity.this).load(selectedImageUri).into(profilePicture);
+                            }
+                        });
             }
         }
 
