fb6298f4f950d0ea26d63c43bba363a589f1e332
TheDorkKnightRises <samriddhabasu@gmail.com>
2017-04-02 03:35:05 +0530

    Replace FAB with labelled button

--- app/src/main/java/smartindia/santas/bloodrelations/activities/MainActivity.java
+++ app/src/main/java/smartindia/santas/bloodrelations/activities/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,6 +18,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
-    FloatingActionButton fab;
+    Button fab;
 
     RecyclerView recyclerView;
     LinearLayoutManager linearLayoutManager;
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.nvView);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.drawer_open,R.string.drawer_close);
 
-        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab = (Button) findViewById(R.id.fab);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
