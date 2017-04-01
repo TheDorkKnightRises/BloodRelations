552887a1f49249c4e66468118199c9a833671825
TheDorkKnightRises <samriddhabasu@gmail.com>
2017-04-01 20:05:03 +0530

    Added MyLocation feature

--- app/src/main/java/smartindia/santas/bloodrelations/activities/LocateActivity.java
+++ app/src/main/java/smartindia/santas/bloodrelations/activities/LocateActivity.java
@@ -1,7 +1,16 @@
 package smartindia.santas.bloodrelations.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.PermissionChecker;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +23,29 @@ import smartindia.santas.bloodrelations.R;
 
 public class LocateActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int LOCATION_REQUEST_CODE = 123;
     private GoogleMap mMap;
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +71,54 @@ public class LocateActivity extends FragmentActivity implements OnMapReadyCallba
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PermissionChecker.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else mMap.setMyLocationEnabled(true);
+
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                if (!isLocationEnabled(LocateActivity.this))
+                    Toast.makeText(LocateActivity.this, "Turn on Location Services", Toast.LENGTH_LONG).show();
+                else if (mMap.isMyLocationEnabled() && mMap.getMyLocation() != null) {
+                    LatLng myLocation = new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
+                    addMarkers(myLocation);
+                }
+                return true;
+            }
+        });
+
+        if (mMap.isMyLocationEnabled() && mMap.getMyLocation() != null) {
+            LatLng myLocation = new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
+            addMarkers(myLocation);
+        }
+
+    }
+
+    private void addMarkers(LatLng location) {
+        mMap.clear();
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 14f));
+        LatLng loc1 = new LatLng(location.latitude - 0.0030, location.longitude + 0.0087);
+        mMap.addMarker(new MarkerOptions().position(loc1).title("Blood Bank"));
+        LatLng loc2 = new LatLng(location.latitude + 0.0057, location.longitude + 0.0092);
+        mMap.addMarker(new MarkerOptions().position(loc2).title("Blood Bank"));
+        LatLng loc3 = new LatLng(location.latitude - 0.0009, location.longitude - 0.0025);
+        mMap.addMarker(new MarkerOptions().position(loc3).title("Blood Bank"));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults[0] == PermissionChecker.PERMISSION_GRANTED)
+                try {
+                    mMap.setMyLocationEnabled(true);
+                } catch (SecurityException e) {
+                    Toast.makeText(this, "Location permission is required to show your location on the map", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            else
+                Toast.makeText(this, "Location permission is required to show your location on the map", Toast.LENGTH_LONG).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
