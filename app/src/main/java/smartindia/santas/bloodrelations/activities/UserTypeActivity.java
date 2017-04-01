d94099c97402c36e0e071e6bba882967155507c4
SaiArrow <saireddy02091997@gmail.com>
2017-04-01 15:26:57 +0530

    Minor

--- app/src/main/java/smartindia/santas/bloodrelations/activities/UserTypeActivity.java
+++ app/src/main/java/smartindia/santas/bloodrelations/activities/UserTypeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +14,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import smartindia.santas.bloodrelations.Constants;
 import smartindia.santas.bloodrelations.R;
+
+import static android.R.attr.button;
 
 public class UserTypeActivity extends AppCompatActivity {
     SharedPreferences prefs;
@@ -31,33 +34,30 @@ public class UserTypeActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
+        final Button b = (Button) findViewById(R.id.Yes);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                        isBloodBank = true;
+                        push();
+                        setsharedpreference();
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
 
+            }
 
-
-    }
-
-    public void onClick(Button b) {
-        switch (b.getId()) {
-
-            case R.id.Yes:
-                isBloodBank = true;
-                push();
-                setsharedpreference();
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
-                break;
-            case R.id.No:
-                isBloodBank = false;
-                push();
-                setsharedpreference();
-                Intent j = new Intent(getApplicationContext(), BloodBankListActivity.class);
-                startActivity(j);
-
-        }
-
+        });
+        final Button b1 = (Button) findViewById(R.id.No);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+        isBloodBank = false;
+        push();
+        setsharedpreference();
+        Intent j = new Intent(getApplicationContext(), BloodBankListActivity.class);
+        startActivity(j);
 
     }
-
+    });
+    }
     public void setsharedpreference() {
         prefs = getSharedPreferences(Constants.PREFS, MODE_PRIVATE);
         editor = prefs.edit();
@@ -72,3 +72,5 @@ public class UserTypeActivity extends AppCompatActivity {
 
     }
 }
+
+
